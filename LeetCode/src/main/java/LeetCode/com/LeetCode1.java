1c0205abcf1b52ca1a4859e4db9b33428dabb0d2
User <User@Lenovo>
2024-01-26 12:27:08 +0100

    initCommit

---
+++ LeetCode/src/main/java/LeetCode/com/LeetCode1.java
@@ -0,0 +1,34 @@
+package LeetCode.com;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode1 {
+
+	
+		public static void main(String[] args) { 
+			int[] nums = {5,2,4};
+			 int target = 6;
+			 LeetCode1 leetCode1 = new LeetCode1();
+			 int[] a = leetCode1.twoSum(nums, target);
+			 
+			 System.out.println(Arrays.toString(a));
+
+			}
+		 public int[] twoSum(int[] nums, int target) {
+			 
+			 Map<Integer, Integer> complements = new HashMap<>() ;
+			 for	(int i = 0; i < nums.length; i++){
+			 Integer complementIndex = complements.get(nums[i]);
+			 if(complementIndex !=null) {
+				 return new int[] {i,complementIndex};
+			 }
+			 complements.put(target-nums[i], i);
+		 }
+			 return nums;
+		 }
+		 
+	}
+
+
---
+++ LeetCode/src/main/java/LeetCode/com/LeetCode2.java
@@ -0,0 +1,10 @@
+package LeetCode.com;
+
+public class LeetCode2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
